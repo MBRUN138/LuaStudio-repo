f966910f303b543800336dba6bdeb2fe8bb98429
mbrun138 <mbrun138@students.bju.edu>
2018-03-29 20:15:48 -0400

    update raycast script

--- RaycastingScripts/GunScript.lua
+++ RaycastingScripts/GunScript.lua
@@ -6,7 +6,7 @@ local mouse = player:GetMouse()
 local tool = script.Parent
 
 mouse.Button1Down:connect(function()
-	local ray = Ray.new(tool.Barrel.CFrame.p,(mouse.Hit.p-tool.Barrel.CFrame.p).unit * 300)
+	local ray = Ray.new(barrel.CFrame.p,(mouse.Hit.p - barrel.CFrame.p).unit * 300)
 	local part, position = workspace:FindPartOnRay(ray, player.Character, false, true)
 	
 	local beam = Instance.new("Part", workspace)
@@ -18,9 +18,9 @@ mouse.Button1Down:connect(function()
 	beam.Locked = true
 	beam.CanCollide = false
 
-	local distance = (tool.Handle.CFrame.p - position).magnitude
+	local distance = (barrel.CFrame.p - position).magnitude
 	beam.Size = Vector3.new(0.3, 0.3, distance)
-	beam.CFrame = CFrame.new(tool.Handle.CFrame.p, position) * CFrame.new(0, 0, -distance / 2)
+	beam.CFrame = CFrame.new(barrel.CFrame.p, position) * CFrame.new(0, 0, -distance / 2)
 
 	game:GetService("Debris"):AddItem(beam, 0.1)
 	
