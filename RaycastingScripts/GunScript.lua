1d218c38e9fea7edad3bc140e4ab307621e32df1
mbrun97 <mbrun138@students.bju.edu>
2018-04-07 23:50:48 -0400

    Update GunScript.lua

--- RaycastingScripts/GunScript.lua
+++ RaycastingScripts/GunScript.lua
@@ -1,37 +1,38 @@
 local player = game:GetService("Players").LocalPlayer
 local barrel = script.Parent.Barrel
 local handle = script.Parent.Handle
-local damage = 70
+local damage = 100
 local mouse = player:GetMouse()
 local tool = script.Parent
 
-mouse.Button1Down:connect(function()
-	local ray = Ray.new(barrel.CFrame.p,(mouse.Hit.p - barrel.CFrame.p).unit * 300)
-	local part, position = workspace:FindPartOnRay(ray, player.Character, false, true)
+tool.Equipped:connect(function(mouse)
+	mouse.Button1Down:connect(function()
+		local ray = Ray.new(barrel.CFrame.p,(mouse.Hit.p-barrel.CFrame.p).unit * 300)
+		local part, position = workspace:FindPartOnRay(ray, player.Character, false, true)
+		
+		local beam = Instance.new("Part", workspace)
+		beam.BrickColor = BrickColor.new("Bright red")
+		beam.FormFactor = "Custom"
+		beam.Material = "Neon"
+		beam.Transparency = 0.25
+		beam.Anchored = true
+		beam.Locked = true
+		beam.CanCollide = false
 	
-	local beam = Instance.new("Part", workspace)
-	beam.BrickColor = BrickColor.new("Bright red")
-	beam.FormFactor = "Custom"
-	beam.Material = "Neon"
-	beam.Transparency = 0.25
-	beam.Anchored = true
-	beam.Locked = true
-	beam.CanCollide = false
-
-	local distance = (barrel.CFrame.p - position).magnitude
-	beam.Size = Vector3.new(0.3, 0.3, distance)
-	beam.CFrame = CFrame.new(barrel.CFrame.p, position) * CFrame.new(0, 0, -distance / 2)
-
-	game:GetService("Debris"):AddItem(beam, 0.1)
+		local distance = (barrel.CFrame.p - position).magnitude
+		beam.Size = Vector3.new(0.3, 0.3, distance)
+		beam.CFrame = CFrame.new(barrel.CFrame.p, position) * CFrame.new(0, 0, -distance / 2)
 	
-	if part then
-		local humanoid = part.Parent:FindFirstChild("Humanoid")
-		if not humanoid then 
-			humanoid = part.Parent.Parent:FindFirstChild("Humanoid")
-		end
-		if humanoid then
-			humanoid:TakeDamage(damage)
-		end
-	end	
+		game:GetService("Debris"):AddItem(beam, 0.1)
+		
+		if part then
+			local humanoid = part.Parent:FindFirstChild("Humanoid")
+			if not humanoid then 
+				humanoid = part.Parent.Parent:FindFirstChild("Humanoid")
+			end
+			if humanoid then
+				humanoid:TakeDamage(damage)
+			end
+		end	
+	end)
 end)
-
