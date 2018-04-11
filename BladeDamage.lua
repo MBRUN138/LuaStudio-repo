708f95c58ca85a2e15a35e714bfdfc4cf6f886be
mbrun97 <mbrun138@students.bju.edu>
2018-04-11 15:57:44 -0400

    Update BladeDamage.lua

--- BladeDamage.lua
+++ BladeDamage.lua
@@ -1,9 +1,12 @@
 local player = game:GetService(“Players”).LocalPlayer
 local blade = script.Parent
 local damage = 25
-
+local CanHit = true
 blade.Touched:connect(function(part)
-    
+    local humanoid = part.Parent.FindFirstChild(“Humanoid”)
+    if humanoid then
+        humanoid:TakeDamage(damage)
+        
 end)
 
 
