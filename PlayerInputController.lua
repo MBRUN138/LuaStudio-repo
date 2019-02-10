e8538b9ced412aedd7000acf9242f78b4a24821f
mbrun97 <mbrun138@students.bju.edu>
2019-02-10 12:00:33 -0500

    Update PlayerInputController.lua

--- PlayerInputController.lua
+++ PlayerInputController.lua
@@ -1,1 +1,15 @@
-pass
+local ContextActionService = game:GetService("ContextActionService")
+ 
+-- Setting up the action handling function
+local function handleAction(actionName, inputState, inputObj)
+    if inputState == Enum.UserInputState.Begin then
+        print("Handling action: " .. actionName)
+    end
+ 
+	-- Since this function does not return anything, this handler will
+	-- "sink" the input and no other action handlers will be called after
+	-- this one.
+end
+ 
+-- Bind the action to the handler
+ContextActionService:BindAction("BoundAction", handleAction, false, Enum.KeyCode.F)
